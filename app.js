a85a7ca8ff2955d839689911deb22a9eb4dd51ff
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2021-07-13 19:14:38 -0500

    Añadiendo codigo necesario para el funcionamiento e interpretacion de los datos para la IA.

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const input_colorPicker = document.getElementById('color-picker');
 
+const boxExample = document.getElementById('colorElement');
+
 const pickerInstance = new JSColor(input_colorPicker, {});
 
-input_colorPicker.addEventListener('input', (e) => {
-    console.log('Color is updating');
-    let colorValue = input_colorPicker.value;
-    console.log(pickerInstance.toRGBString(colorValue));
+let suggestedFGColor;
+
+const convertToRGBArray = (input) => {
+    let inner = input.replace('rgb(', '');
+    let newValue = inner.replace(')', '').split(",");
+    return newValue;
+}
+
+input_colorPicker.addEventListener('input', () => {
+    let color = convertToRGBArray(pickerInstance.toRGBString(input_colorPicker.value));
+    suggestedFGColor = update(color);
+    boxExample.style.backgroundColor = input_colorPicker.value;
+    if (suggestedFGColor > .5) boxExample.style.color = 'white';
+    else boxExample.style.color = 'black';
+    
 });
