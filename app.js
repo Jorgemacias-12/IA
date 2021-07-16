785345ebd819ffbc12ff4fccbb1b11b9afb54b44
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2021-07-16 11:15:06 -0500

    Actualizando el archivo para añadir eventos y llamar a funciones que se encargaran del funcionamiento.

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
-const input_colorPicker = document.getElementById('color-picker');
+const colorPicker = document.getElementById('color-picker');
+const colorBox = document.getElementById('colorElement');
 
-const boxExample = document.getElementById('colorElement');
-
-const pickerInstance = new JSColor(input_colorPicker, {});
-
-let suggestedFGColor;
-
-const convertToRGBArray = (input) => {
-    let inner = input.replace('rgb(', '');
-    let newValue = inner.replace(')', '').split(",");
-    return newValue;
+window.onload = () => {
+    document.getElementById('tab-1').addEventListener('click', (e) => { changeTab(e) });
+    document.getElementById('tab-2').addEventListener('click', (e) => { changeTab(e) });
+    colorPicker.addEventListener('input', (e) => {ColorInput(e.target)});
 }
-
-input_colorPicker.addEventListener('input', () => {
-    let color = convertToRGBArray(pickerInstance.toRGBString(input_colorPicker.value));
-    suggestedFGColor = update(color);
-    boxExample.style.backgroundColor = input_colorPicker.value;
-    if (suggestedFGColor > .5) boxExample.style.color = 'white';
-    else boxExample.style.color = 'black';
-    
-});
