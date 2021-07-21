05f09bb789f9336ab673579dc4765bff312621c9
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2021-07-20 20:42:52 -0500

    Añadiendo codigo que carga y añade eventos al color picker, y al checkbox que permite cambiar tema a tema.

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,7 @@ const colorBox = document.getElementById('colorElement');
 window.onload = () => {
     document.getElementById('tab-1').addEventListener('click', (e) => { changeTab(e) });
     document.getElementById('tab-2').addEventListener('click', (e) => { changeTab(e) });
-    colorPicker.addEventListener('input', (e) => {ColorInput(e.target)});
+    colorPicker.addEventListener('input', (e) => { ColorInput(e.target) });
     enableDarkMode(document.getElementById('checkbox').checked);
-    document.getElementById('checkbox').addEventListener('click', (e) => {enableDarkMode(e.target.checked)});
+    document.getElementById('checkbox').addEventListener('click', (e) => { enableDarkMode(e.target.checked) });
 }
