89dbb83e87c9631e6f892f938f12c7115bdfd2e9
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2021-07-21 14:20:08 -0500

    Añadiendo cambios de la rama de desarollo.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,18 @@
-const colorPicker = document.getElementById("color-picker"), colorBox = document.getElementById("colorElement"); window.onload = (() => { document.getElementById("tab-1").addEventListener("click", e => { changeTab(e) }), document.getElementById("tab-2").addEventListener("click", e => { changeTab(e) }), colorPicker.addEventListener("input", e => { ColorInput(e.target) }), enableDarkMode(document.getElementById("checkbox").checked), document.getElementById("checkbox").addEventListener("click", e => { enableDarkMode(e.target.checked) }) });
+const colorPicker = document.getElementById("color-picker");
+const colorBox = document.getElementById("colorElement");
+
+window.onload = () => {
+    document.getElementById("tab-1").addEventListener("click", (e) => {
+        changeTab(e);
+    }),
+    document.getElementById("tab-2").addEventListener("click", (e) => {
+        changeTab(e);
+    }),
+    colorPicker.addEventListener("input", (e) => {
+        ColorInput(e.target);
+    }),
+    enableDarkMode(document.getElementById("checkbox").checked),
+    document.getElementById("checkbox").addEventListener("click", (e) => {
+        enableDarkMode(e.target.checked);
+    });
+};
--- scripts/network.js
+++ scripts/network.js
@@ -1,1 +1,13 @@
-let network=new brain.NeuralNetwork;function update(e){let r={red:e[0]/255,green:e[1]/255,blue:e[2]/255};return network.run(r).color}network.train([{input:{red:0,green:0,blue:0},output:{color:1}},{input:{red:0,green:1,blue:0},output:{color:0}},{input:{red:0,green:0,blue:.34},output:{color:1}},{input:{red:1,green:0,blue:0},output:{color:1}},{input:{red:0,green:.4,blue:1},output:{color:1}},{input:{red:1,green:1,blue:1},output:{color:0}}]);
+let network = new brain.NeuralNetwork();
+function update(e) {
+    let r = { red: e[0] / 255, green: e[1] / 255, blue: e[2] / 255 };
+    return network.run(r).color;
+}
+network.train([
+    { input: { red: 0, green: 0, blue: 0 }, output: { color: 1 } },
+    { input: { red: 0, green: 1, blue: 0 }, output: { color: 0 } },
+    { input: { red: 0, green: 0, blue: 0.34 }, output: { color: 1 } },
+    { input: { red: 1, green: 0, blue: 0 }, output: { color: 1 } },
+    { input: { red: 0, green: 0.4, blue: 1 }, output: { color: 1 } },
+    { input: { red: 1, green: 1, blue: 1 }, output: { color: 0 } },
+]);
