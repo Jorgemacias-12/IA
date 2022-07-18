7e257e010a117f53b315c2a32d10eb00dc1d8df0
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2022-07-18 05:32:34 -0500

    Cambio menor, salto de linea #3
    	closes #3

--- scripts/network.js
+++ scripts/network.js
@@ -4,6 +4,7 @@ function update(e) {
     let r = { red: e[0] / 255, green: e[1] / 255, blue: e[2] / 255 };
     return network.run(r).color;
 }
+
 network.train([
     { input: { red: 0, green: 0, blue: 0 }, output: { color: 1 } },
     { input: { red: 0, green: 1, blue: 0 }, output: { color: 0 } },
